99171101bebc454b06d0fd28aef6f28284e02249
Nkanyiso28 <praymoreqiniso1@gmail.com>
2026-04-14 03:45:26 +0200

    Clean requirements

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.overview,name='overview'),#default page
-    path('sessions/', views.dashboard_view,name='dashboard'),
+    path('', views.dashboard_view,name='dashboard'),
+    path('overview', views.overview,name='overview'),#default page
     path('delete/<int:id>/', views.delete_session,name='delete_session'),
    # path('export/', views.views.eport_excel,name='export_excel'),
     
