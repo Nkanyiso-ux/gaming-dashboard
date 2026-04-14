0ca0bd1fce3dded169073cbb137e02df4e249e53
Nkanyiso-ux <madikizelanonkanyiso0@gmail.com>
2026-04-14 19:59:31 +0200

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,11 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard_view,name='dashboard'),
-<<<<<<< HEAD
     path('overview', views.overview,name='overview'),#default page
-=======
     path('overview/', views.overview,name='overview'),#default page
->>>>>>> 7ca17e07060172a71a63ea8aaffc785142cf3297
     path('delete/<int:id>/', views.delete_session,name='delete_session'),
    # path('export/', views.views.eport_excel,name='export_excel'),
     
