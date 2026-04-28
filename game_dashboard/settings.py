ae587ac79200aa926d095581a6043a05039a9054
Nkanyiso28 <praymoreqiniso1@gmail.com>
2026-04-28 10:33:34 +0200

    update

--- game_dashboard/settings.py
+++ game_dashboard/settings.py
@@ -123,4 +123,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
